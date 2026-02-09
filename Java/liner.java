22e875cb13665bd17f823b8c90c86b23dbcea00f
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2026-02-09 21:57:32 +0800

    Implement linear and binary search algorithms

--- Java/liner.java
+++ Java/liner.java
@@ -1,1 +1,51 @@
-#insert text
+// SearchingAlgorithms.java
+public class SearchingAlgorithms {
+    
+    // Linear Search
+    public int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    // Binary Search (iterative)
+    public int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (arr[mid] == target) {
+                return mid;
+            }
+            
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+    
+    // Binary Search (recursive)
+    public int binarySearchRecursive(int[] arr, int target, int left, int right) {
+        if (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (arr[mid] == target) {
+                return mid;
+            }
+            
+            if (arr[mid] > target) {
+                return binarySearchRecursive(arr, target, left, mid - 1);
+            }
+            return binarySearchRecursive(arr, target, mid + 1, right);
+        }
+        return -1;
+    }
+}
