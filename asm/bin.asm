55eb79a8b96fc3697551314b7ac4b521b566ecce
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2026-03-26 14:08:04 +0800

    Add binary operations example in bin.asm

--- asm/bin.asm
+++ asm/bin.asm
@@ -1,1 +1,16 @@
-k
+section .text
+    global _start
+
+_start:
+    ; Binary operations example
+    mov al, 0b10101010  ; binary literal
+    mov bl, 0b11001100
+    
+    and al, bl          ; AND operation
+    or al, bl           ; OR operation
+    xor al, bl          ; XOR operation
+    
+    ; Exit
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
