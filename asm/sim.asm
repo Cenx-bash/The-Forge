26fa37ab2aa01ca9aaec9e611f01fa431b5f8ca5
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2026-03-26 14:09:30 +0800

    Update sim.asm

--- asm/sim.asm
+++ asm/sim.asm
@@ -1,1 +1,27 @@
+section .data
+    registers times 16 dw 0
+    pc dw 0              ; program counter
+    running db 1
 
+section .text
+    global _start
+
+_start:
+    ; Simple CPU simulation loop
+sim_loop:
+    cmp byte [running], 0
+    je done
+    
+    ; Fetch instruction
+    mov bx, [pc]
+    ; Decode and execute would go here
+    ; ... simulation logic ...
+    
+    inc word [pc]        ; increment PC
+    jmp sim_loop
+
+done:
+    ; Exit
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
