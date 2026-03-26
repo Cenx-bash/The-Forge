bd9bd66dc5af05698c6e7fccaa3e5798ba1f866e
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2026-03-26 14:08:40 +0800

    Implement bit manipulation examples in bite.asm
    Added bit manipulation examples including setting, clearing, and testing bits, as well as rotate operations.

--- asm/bite.asm
+++ asm/bite.asm
@@ -1,1 +1,25 @@
+section .text
+    global _start
 
+_start:
+    ; Bit manipulation examples
+    mov al, 0xFF        ; 11111111
+    mov bl, 0x0F        ; 00001111
+    
+    ; Set bit 3
+    bts ax, 3
+    
+    ; Clear bit 5
+    btr ax, 5
+    
+    ; Test bit 4
+    bt ax, 4
+    
+    ; Rotate operations
+    rol al, 1           ; rotate left
+    ror al, 1           ; rotate right
+    
+    ; Exit
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
