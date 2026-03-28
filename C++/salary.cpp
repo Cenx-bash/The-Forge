b98149f31d374df86f3ad9c665115054664257a5
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2026-03-28 11:24:09 +0800

    Create salary.cpp

---
+++ C++/salary.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double sales, salary;
+
+    cout << "Enter sales in dollars (-1 to end): ";
+    cin >> sales;
+
+    while (sales != -1) {
+        salary = 200 + (0.09 * sales);
+
+        cout << "Salary is: $" << salary << endl;
+
+        cout << "\nEnter sales in dollars (-1 to end): ";
+        cin >> sales;
+    }
+
+    return 0;
+}
